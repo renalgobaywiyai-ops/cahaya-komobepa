3130503405206e51bb1b6603b8f1306e07bf7839
renalgobaywiyai-ops <renalgobaywiyai@gmail.com>
2026-05-24 00:22:45 +0900

    Fix formatting and typos in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 
-st.set_page_config(page_title="Cahya Komobepa")
+st.set_page_config(page_title="Cahya
+Komobepa")
+
 st.title("Website Cahaya Komobepa")
 st.subheader("Selamat datang di website Cahay Komobepa")
 menu = st.sidebar.selectbox(
     "Pilih Menuh",
-    ["Beranda", Tentang", "kontak"]
+    ["Beranda", Tentang","Kontak"]
 )
 if menu == "Beranda":
     st.write("Ini adalah halaman utama website.")
@@ -15,8 +17,8 @@ elif menu == "Tentang":
 informasi dan publikasi.")
 
 elif menu == "Kontak":
-    st.write(Email: renalgobaywiyai@gmail.com")
-    st.write(WhatsApp: 08xxxxxxxxxx")
+st.write("Email: renalgobaywiyai@gmail.com")
+st.write(WhatsApp: 08xxxxxxxxxx")
 st.image(
 "https://cahaya-komobepa.io/images/brand/streamilit-log
 o-primary-colormark-darktext.png",
