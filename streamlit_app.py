3ca0ef88db5e69dbbdc912443112c254c2e58ac0
renalgobaywiyai-ops <renalgobaywiyai@gmail.com>
2026-05-23 23:20:08 +0900

    Update aplikasi Cahaya Komobepa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(page_title="Cahya Komobepa")
+st.title("Website Cahaya Komobepa")
+st.subheader("Selamat datang di website Cahay Komobepa")
+menu = st.sidebar.selectbox(
+    "Pilih Menuh",
+    ["Beranda", Tentang", "Kontak"]
 )
+if menu == "Beranda":
+    st.write("Ini adalah halaman utama website.")
+
+elif menu == "Tentang":
+     st.write("Cahaya Komobepa adalah website
+informasi dan publikasi.")
+
+elif menu == "Kontak":
+    st.write(Email: renalgobaywiyai@gmail.com")
+    st.write(WhatsApp: 08xxxxxxxxxx")
+st.image(
+"https://cahaya-komobepa.io/images/brand/streamilit-log
+o-primary-colormark-darktext.png",
+   width=200
+)
+                            
